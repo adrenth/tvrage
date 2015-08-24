64e818f567f1964e64e2ef939a989762912c4f76
Alwin Drenth <adrenth@gmail.com>
2015-08-24 20:28:40 +0200

    Add test suite

---
+++ tests/Adrenth/Tests/AdrenthTestCase.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Adrenth\Tests;
+
+/**
+ * Base class for all test cases.
+ *
+ * @category Tvrage
+ * @package  Adrenth\Tvrage
+ * @author   Alwin Drenth <adrenth@gmail.com>
+ * @license  http://opensource.org/licenses/MIT The MIT License (MIT)
+ * @link     https://github.com/adrenth/tvrage
+ */
+abstract class AdrenthTestCase extends \PHPUnit_Framework_TestCase
+{
+
+}
